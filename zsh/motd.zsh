7f94063095ad40aeb852707c1f01205b12cb60bf
LkeMitchll <lke.mitchll@gmail.com>
2015-06-18 16:42:45 +0100

    Moar cows.

--- zsh/motd.zsh
+++ zsh/motd.zsh
@@ -1,10 +1,8 @@
 LINE='==========================================='
 
 # display big gay unicorn
-#cat ~/.zsh/welcome.txt | lolcat
-echo ""
-echo "ಠ_ಠ"
-echo ""
+cowsay "Hello!"
+echo $LINE
 # display time and host
 echo "Time: $(date)"
 echo "Host: $(whoami) @ $(hostname)"
