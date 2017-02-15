313993653f4b5fbeae52041801f96bee56842807
LkeMitchll <lke.mitchll@gmail.com>
2017-02-15 11:46:32 +0000

    Fish: Make the $PATH easier to manage

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,1 @@
-set -x PATH /usr/local/bin $PATH
-set -x PATH /usr/bin $PATH
-set -x PATH bin $PATH
-set -x PATH /usr/sbin $PATH
-set -x PATH /sbin $PATH
-set -x PATH ~/.bin $PATH
-
-set -x PATH $HOME/.rbenv/shims $PATH
-status --is-interactive; and source (rbenv init -|psub)
-
 source $HOME/.aliases
