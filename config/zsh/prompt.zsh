ec4bf5f3708867efe6016ac1bc4e70b1c8e77252
LkeMitchll <mail@luke.fail>
2018-04-26 10:35:56 +0100

    Term: Add kitty config

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -1,6 +1,6 @@
 PREFIX="%{$fg[cyan]%}╭─-<<"
 CWD="%{$fg[magenta]%}%~"
-SUFFIX="%{$fg[cyan]%}╰─--> %{$fg[white]%}"
+SUFFIX="%{$fg[cyan]%}╰─->> %{$fg[white]%}"
 NEWLINE=$'\n'
 
 PROMPT="${PREFIX} ${CWD}${NEWLINE}${SUFFIX}"
