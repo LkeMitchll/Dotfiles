91d1fc5d1eecad0e1bc003fac9183a832f391902
LkeMitchll <mail@luke.fail>
2018-08-22 11:16:52 +0100

    Phoenix: Refactor config

--- config/phoenix/phoenix.js
+++ config/phoenix/phoenix.js
@@ -1,59 +1,64 @@
+/* eslint-disable */
 Phoenix.set({ openAtLogin: true });
 
-var prefix = ['ctrl', 'alt'];
+var prefix = ["ctrl", "alt"];
+var scr = Screen.main().visibleFrameInRectangle();
+var win = Window.focused();
 
-Key.on('1', prefix, () => {
-  var screen = Screen.main().flippedVisibleFrame();
-  var window = Window.focused();
-
-  if (window) {
-    window.setTopLeft({
-      x: 0,
-      y: 0
-    });
-
-    window.setSize({
-      width: screen.width / 2,
-      height: screen.height
-    });
+var windowLocations = {
+  left: {
+    x: 0,
+    y: 0,
+    width: scr.width / 2,
+    height: scr.height
+  },
+  right: {
+    x: scr.width / 2,
+    y: 0,
+    width: scr.width / 2,
+    height: scr.height
+  },
+  full: {
+    x: 0,
+    y: 0,
+    width: scr.width,
+    height: scr.height
+  },
+  center: {
+    x: scr.x + scr.width / 2 - win.frame().width / 2,
+    y: scr.y + scr.height / 2 - win.frame().height / 2,
   }
+};
+
+// Left half of the screen
+Key.on("1", prefix, () => {
+  Window.focused().setFrame(windowLocations.left);
 });
 
-Key.on('2', prefix, () => {
-  var screen = Screen.main().flippedVisibleFrame();
-  var window = Window.focused();
-
-  if (window) {
-    window.setTopLeft({
-      x: screen.width / 2,
-      y: 0
-    });
-
-    window.setSize({
-      width: screen.width / 2,
-      height: screen.height
-    });
-  }
+// Right half of the screen
+Key.on("2", prefix, () => {
+  Window.focused().setFrame(windowLocations.right);
 });
 
-Key.on('c', prefix, () => {
-  var screen = Screen.main().flippedVisibleFrame();
-  var window = Window.focused();
+// Full screen
+Key.on("f", prefix, () => {
+  Window.focused().setFrame(windowLocations.full);
+});
 
-  if (window) {
-    window.setTopLeft({
-      x: screen.x + (screen.width / 2) - (window.frame().width / 2),
-      y: 0
-    });
+// Centered
+Key.on("z", prefix, function() {
+  Window.focused().setTopLeft({
+    x: scr.x + (scr.width / 2) - (Window.focused().frame().width / 2),
+    y: scr.y + (scr.height / 2) - (Window.focused().frame().height / 2)
+  });
+});
 
-    window.setSize({
-      width: screen.width / 2,
-      height: screen.height
-    });
+// One-third centered
+Key.on("c", prefix, () => {
+  Window.focused().setFrame(windowLocations.left);
 
-    window.setTopLeft({
-      x: screen.x + (screen.width / 2) - (window.frame().width / 2),
-      y: 0
-    });
-  }
+  Window.focused().setTopLeft({
+    x: scr.x + (scr.width / 2) - (Window.focused().frame().width / 2),
+    y: scr.y + (scr.height / 2) - (Window.focused().frame().height / 2)
+  });
 });
