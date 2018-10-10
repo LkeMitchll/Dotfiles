3d6df06eed6281b004e4eae597a988606fe18790
LkeMitchll <mail@luke.fail>
2018-10-10 12:22:19 +0100

    Zsh: refactor some functions & tidy up

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -7,21 +7,19 @@ alias vi='nvim'
 alias z='source ~/.zshrc'
 alias finder='open .'
 alias vim-update="nvim '+PlugClean' '+PlugUpdate' '+PlugUpgrade' '+qa!'"
-# Web dev
 alias yr="yarn run"
 alias be="bundle exec"
-# Git
 alias git="hub"
 alias g="git"
 
 # Kitty
-tab() {
+kitty-tab() {
   kitty @ set-tab-title $1; clear
 }
 
 session() {
   cd $1
-  tab $2; clear
+  kitty-tab $2; clear
   kitty @ new-window --cwd=$PWD --keep-focus; clear
 }
 
@@ -43,7 +41,6 @@ uatt() {
   mas upgrade
   echo $SEPERATOR
   vim-update
-  echo "Vim plugins up to date."
   echo $SEPERATOR
   waiter 5
 }
