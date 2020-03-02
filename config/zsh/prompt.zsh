009b4e77add7146516708ddb0ec463e3b0207ff3
LkeMitchll <mail@luke.fail>
2020-03-02 16:17:34 +0000

    ZSH: Simplify prompt

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -1,9 +1,9 @@
-# ╭── /bin
-# ╰─> ls
+# ╭── ~/.config
+# ╰── ls
 
 PREFIX="%{$fg[blue]%}╭──"
-CWD="%{$fg[magenta]%}%~"
-SUFFIX="%{$fg[blue]%}╰─> "
+CWD="%{$fg[magenta]%}%~ %{$fg[blue]%}"
+SUFFIX="%{$fg[blue]%}╰── "
 NEWLINE=$'\n'
 
 PROMPT="${PREFIX} ${CWD}${NEWLINE}${SUFFIX}%{$fg[white]%}"
