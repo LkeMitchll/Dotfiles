5d0ee75ddd0257e753e07586387d9348346b9c00
LkeMitchll <mail@luke.fail>
2020-05-01 08:29:51 +0100

    Vim: fix uninstall script

--- config/fish/functions/nvim-remove.fish
+++ config/fish/functions/nvim-remove.fish
@@ -4,9 +4,9 @@ function nvim-remove
 
   if test -n $argv[1]
     echo "nvim: Uninstalling" $argv[1]
-    git submodule deinit config/nvim/pack/interrobang/$argv[1]
-    git rm config/nvim/pack/interrobang/$argv[1]
-    rm -Rf .git/modules/config/nvim/pack/interrobang/$argv[1]
+    git submodule deinit config/nvim/pack/interrobang/start/$argv[1]
+    git rm config/nvim/pack/interrobang/start/$argv[1]
+    rm -Rf .git/modules/config/nvim/pack/interrobang/start/$argv[1]
   end
 
   cd -
