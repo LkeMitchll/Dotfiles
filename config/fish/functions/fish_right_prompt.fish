f4a468ccb17a9387fc9af585a5ec1176106b10c3
LkeMitchll <mail@luke.fail>
2020-10-30 10:22:50 +0000

    Fish: trim the git branch if it's too long

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,4 +1,12 @@
 function fish_right_prompt
-  set -l git_branch (hub branch ^/dev/null | sed -n '/\* /s///p')
-  echo (set_color brblack)$git_branch
+  set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+  set -l branch_length (string length $git_branch)
+
+  if test -n "$git_branch"
+    if test "$branch_length" -gt 15
+      echo (set_color brblack)(string sub --length 15 $git_branch)"..."
+    else
+      echo (set_color brblack)$git_branch
+    end
+  end
 end
