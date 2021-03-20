9aaabb721b5ec2a8878f6f1edab20f97669862c0
LkeMitchll <mail@luke.fail>
2021-03-20 16:54:51 +0000

    Try native LSP instead of COC

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,9 @@
+local set_keymap = vim.api.nvim_set_keymap
+
 vim.g.mapleader = " "
 vim.o.clipboard = "unnamed"
 vim.o.cursorline = true
-vim.o.relativenumber = true
+vim.wo.relativenumber = true
 vim.wo.list = true
 vim.o.listchars = "eol:¬,space:·,trail:"
 vim.o.splitbelow = true
@@ -11,8 +13,10 @@ vim.o.shiftround = true
 vim.o.softtabstop = 2
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.completeopt = "menuone,noselect"
 vim.cmd("colorscheme interrobang")
 
+-- nvim-treesitter
 local treesitter = require 'nvim-treesitter.configs'
 treesitter.setup {
   ensure_installed = 'maintained',
@@ -20,14 +24,36 @@ treesitter.setup {
   indent = { enable = true }
 }
 
--- vim-fugitive
-vim.api.nvim_set_keymap("n", "<leader>gs", ":G<CR>", {})
+-- nvim-lsp
+local lspconfig = require 'lspconfig'
+lspconfig.tsserver.setup{}
+lspconfig.cssls.setup{}
+lspconfig.html.setup{
+  filetypes = {'html', 'htmldjango'}
+}
 
--- coc.nvim
-vim.api.nvim_set_keymap("i", "<C-j>", "<Plug>(coc-snippets-expand-jump)", {})
-vim.api.nvim_set_keymap("n", "<C-t>", ":CocList files<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>ag", ":CocList grep<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>b", ":CocList buffers<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>cd", "<Plug>(coc-definition)", {})
-vim.api.nvim_set_keymap("n", "<leader>d", ":CocList diagnostics<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>p", ":call CocAction('format')<CR>", {})
+set_keymap('n', '<leader>cd', '<Cmd>lua vim.lsp.buf.definition()<CR>', {})
+set_keymap('n', '<leader>d', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', {})
+
+-- nvim-compe
+require'compe'.setup {
+  source = {
+    path = true;
+    buffer = true;
+    nvim_lsp = true;
+    ultisnips = true;
+    nvim_treesitter = true;
+  };
+}
+set_keymap("i", "<CR>", "compe#confirm('<CR>')", { expr = true; noremap = true })
+
+-- vim-fugitive
+set_keymap("n", "<leader>gs", ":G<CR>", {})
+
+-- fzf.vim
+set_keymap("n", "<C-t>", ":Telescope find_files<CR>", {})
+set_keymap("n", "<leader>ag", ":Telescope live_grep<CR>", {})
+set_keymap("n", "<leader>b", ":Telescope buffers<CR>", {})
+
+-- neoformat
+set_keymap("n", "<leader>p", ":Neoformat<CR>", {})
