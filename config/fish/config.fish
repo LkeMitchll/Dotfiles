943c3de648f3d51c9510f8a20959961dd4f0434f
LkeMitchll <mail@luke.fail>
2021-04-01 15:09:38 +0100

    Fish: Move project-specific settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 set -e PATH
-set -gx PATH /usr/local/bin /usr/sbin /sbin /usr/bin /bin /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+set -gx PATH /usr/local/bin /usr/sbin /sbin /usr/bin /bin $PATH
 
 set -gx EDITOR nvim
 set -gx HOMEBREW_BUNDLE_NO_LOCK 1
@@ -17,9 +17,5 @@ source ~/.asdf/asdf.fish
 # FZF
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
 
-# Android
-set -gx ANDROID_HOME $HOME/Library/Android/sdk
-set -gx PATH $PATH $ANDROID_HOME/emulator
-set -gx PATH $PATH $ANDROID_HOME/tools
-set -gx PATH $PATH $ANDROID_HOME/tools/bin
-set -gx PATH $PATH $ANDROID_HOME/platform-tools
+# Local overrides
+source ~/.config/fish/config.local.fish
