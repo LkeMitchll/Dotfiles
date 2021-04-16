6c091db12e5f870dae96af87ef303acb85b4fbc7
LkeMitchll <mail@luke.fail>
2021-04-16 14:53:14 +0100

    Try emmet-ls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -28,7 +28,25 @@ treesitter.setup {
 
 -- nvim-lsp
 local lspconfig = require "lspconfig"
+local configs = require "lspconfig/configs"
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+configs.emmet_ls = {
+  default_config = {
+    cmd = {"emmet-ls", "--stdio"},
+    filetypes = {"html", "css"},
+    root_dir = function()
+      return vim.loop.cwd()
+    end,
+    settings = {}
+  }
+}
+
+lspconfig.emmet_ls.setup {
+  on_attach = on_attach
+}
 lspconfig.cssls.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.html.setup {filetypes = {"html", "htmldjango", "eruby"}}
