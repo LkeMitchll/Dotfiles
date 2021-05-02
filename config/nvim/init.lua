24b2edbf9b4299b2245973e90015888e9be256cc
LkeMitchll <mail@luke.fail>
2021-05-02 16:53:45 +0100

    Vim: Disable incremental selection

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -66,16 +66,7 @@ local treesitter = require "nvim-treesitter.configs"
 treesitter.setup {
   ensure_installed = "maintained",
   highlight = {enable = true},
-  indent = {enable = true},
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm"
-    }
-  }
+  indent = {enable = true}
 }
 
 -- Plugin: nvim-lspconfig
