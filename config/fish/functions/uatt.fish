fab7cf5c0bb23c7b5e29789b590c63aca133bdd0
LkeMitchll <mail@luke.fail>
2021-07-30 11:06:16 +0100

    Fish: Add helpful comments to the uatt script

--- config/fish/functions/uatt.fish
+++ config/fish/functions/uatt.fish
@@ -1,23 +1,27 @@
 function uatt
   set uatt_seperator "====================="
 
-  brew bundle --global
-  brew bundle --file ~/.Brewfile.local
+  /* Update homebrew itself */
   brew update
+  /* Install ~/.Brewfile dependencies */
+  brew bundle --global
+  /* Install ~/.Brewfile.local dependencies */
+  brew bundle --file ~/.Brewfile.local
+  /* Upgrade homebrew dependencies */
   brew upgrade
   echo "Packages up-to-date"
   echo $uatt_seperator
-  RCRC=~/.dotfiles/config/rcrc rcup
-  echo "Dotfiles up-to-date"
-  echo $uatt_seperator
+  /* Update fisher plugins */
   fisher update
   echo "Fish plugins up-to-date"
   echo $uatt_seperator
-  asdf update
+  /* Update asdf plugins */
   asdf plugin-update --all
   echo "ASDF plugins up-to-date"
   echo $uatt_seperator
+  /* Install LSP's */
   cd ~/.config/nvim ;and npm install ;and cd -
+  /* Update Neovim plugins */
   nvim +PackerSync
   echo "Neovim plugins up-to-date"
   echo $uatt_seperator
