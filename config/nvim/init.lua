5ce68d68ec97e0721a6c33986dd564859926213f
LkeMitchll <mail@luke.fail>
2021-09-06 10:09:22 +0100

    Vim: Config cleanup
    - Move plugin configs into packer init
    - Fix ":set title"

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,11 +1,13 @@
 local set_option = vim.api.nvim_set_option
 local set_keymap = vim.api.nvim_set_keymap
+local global = vim.g
+local window = vim.wo
 local command = vim.cmd
 
-vim.g.mapleader = " "
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.list = true
+global.mapleader = " "
+window.number = true
+window.relativenumber = true
+window.list = true
 set_option("clipboard", "unnamed")
 set_option("cursorline", true)
 set_option("listchars", "eol:¬,space:·,trail:,extends:#")
@@ -19,7 +21,7 @@ set_option("shiftwidth", 2)
 set_option("completeopt", "menuone,noselect")
 set_option("updatetime", 500)
 set_option("wrap", false)
-set_option("title", true)
+command("set title")
 
 require("packer").startup(function()
     use "wbthomason/packer.nvim"
@@ -47,62 +49,47 @@ require("packer").startup(function()
     use {
         "nvim-treesitter/nvim-treesitter",
         branch = "0.5-compat",
-        run = ":TSUpdate"
+        run = ":TSUpdate",
+        config = function()
+            local treesitter = require "nvim-treesitter.configs"
+            treesitter.setup {
+                ensure_installed = "maintained",
+                highlight = {enable = true},
+                indent = {enable = true}
+            }
+        end
     }
     use {
         "nvim-telescope/telescope.nvim",
         requires = {"nvim-lua/popup.nvim", "nvim-lua/plenary.nvim"}
     }
-    use {"TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim"}
+    use {
+        "TimUntersberger/neogit",
+        requires = "nvim-lua/plenary.nvim",
+        config = function() require("neogit").setup {} end
+    }
 end)
+
+-- LSP
+require("lsp")
+
+-- Theme
+global.tokyonight_style = "night"
+command("colorscheme tokyonight")
+require("lualine").setup {options = {theme = "tokyonight"}}
 
 -- Keymaps
 set_keymap("n", "<C-]>", ":cnext<CR>", {})
 set_keymap("n", "<C-[>", ":cprevious<CR>", {})
 
--- Plugin: tokyonight.nvim
-vim.g.tokyonight_style = "night"
-command "colorscheme tokyonight"
-
--- Plugin: lualine.nvim
-require("lualine").setup {options = {theme = "tokyonight"}}
-
--- Plugin: nvim-treesitter
-local treesitter = require "nvim-treesitter.configs"
-treesitter.setup {
-    ensure_installed = "maintained",
-    highlight = {enable = true},
-    indent = {enable = true}
-}
-
 -- Plugin: nvim-lspconfig
-require("lsp")
-
 set_keymap("n", "<leader>cd", ":lua vim.lsp.buf.definition()<CR>", {})
 set_keymap("n", "<leader>p", ":lua vim.lsp.buf.formatting()<CR>", {})
 
--- Plugin: nvim-kitty-runner
-vim.g.KittySwitchFocus = 1
-vim.g.KittyFocusLayout = "tall:bias=60"
-
 -- Plugin: neogit
-require("neogit").setup {}
 set_keymap("n", "<leader>gs", ":Neogit kind=split<CR>", {})
 
 -- Plugin: telescope.nvim
-local tsactions = require("telescope.actions")
-require("telescope").setup {
-    defaults = {
-        mappings = {
-            i = {
-                -- Open quickfix with tagged files
-                ["<C-z>"] = tsactions.send_selected_to_qflist +
-                    tsactions.open_qflist
-            }
-        }
-    }
-}
-
 set_keymap("n", "<C-t>", ":Telescope find_files<CR>", {})
 set_keymap("n", "<leader>ag", ":Telescope live_grep<CR>", {})
 set_keymap("n", "<leader>b", ":Telescope oldfiles<CR>", {})
@@ -110,5 +97,8 @@ set_keymap("n", "<leader>ca", ":Telescope lsp_code_actions<CR>", {})
 set_keymap("n", "<leader>d", ":Telescope lsp_document_diagnostics<CR>", {})
 set_keymap("n", "<leader>v", ":Telescope commands<CR>", {})
 
--- Plugin: coq_nvim
-vim.g.coq_settings = {auto_start = "shut-up", keymap = {jump_to_mark = "<C-g>"}}
+-- Plugin: COQ
+global.coq_settings = {
+    auto_start = "shut-up",
+    keymap = {jump_to_mark = "<C-g>"}
+}
