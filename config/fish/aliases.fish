2bf4491b5ca79c5292bda80f79fa2f0ceb66614f
LkeMitchll <mail@luke.fail>
2021-10-21 15:02:07 +0100

    Fish: Convert 'rcup' alias back to avoid loop

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -4,7 +4,7 @@ abbr vim "nvim"
 abbr yr "yarn run"
 abbr nr "npm run"
 abbr be "bundle exec"
+abbr rcup "RCRC=~/.dotfiles/config/rcrc rcup"
 
 alias g="hub"
 alias git="hub"
-alias rcup="RCRC=~/.dotfiles/config/rcrc rcup"
