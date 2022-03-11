8b8ff289cbcd344b3bc830206018bddbed192323
LkeMitchll <mail@luke.fail>
2022-03-11 12:35:54 +0000

    Vim: Move plugin configs to packer

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,43 +1,73 @@
-local global = vim.g
-local option = vim.opt
-local command = vim.cmd
-local set_keymap = vim.api.nvim_set_keymap
+global = vim.g
+command = vim.cmd
+option = vim.opt
+set_keymap = vim.api.nvim_set_keymap
 
 global.mapleader = " "
-option.number = true
-option.relativenumber = true
-option.list = true
 option.clipboard = "unnamed"
+option.relativenumber = true
+option.list, option.listchars = true, { space = "·", trail = "~", extends = "#", eol = "¬" }
 option.cursorline = true
-option.listchars = { space = "·", trail = "~", extends = "#", eol = "¬" }
 option.splitbelow = true
 option.smartindent = true
 option.expandtab = true
 option.shiftround = true
-option.softtabstop = 2
-option.tabstop = 2
 option.shiftwidth = 2
 option.completeopt = { "menuone", "noselect" }
-option.updatetime = 500
-option.wrap = false
-option.title = true
 
 require("packer").startup(function()
   use("wbthomason/packer.nvim")
   use("nvim-lua/plenary.nvim")
   --
-  use("folke/tokyonight.nvim")
-  use("ggandor/lightspeed.nvim")
-  use("lkemitchll/vim-kitty-runner")
-  use("neovim/nvim-lspconfig")
-  use("jose-elias-alvarez/null-ls.nvim")
-  use("tpope/vim-surround")
-  use({ "knubie/vim-kitty-navigator", run = "cp ./*.py ~/.config/kitty/" })
+  use({
+    "folke/tokyonight.nvim",
+    config = function()
+      global.tokyonight_style = "night"
+      command("colorscheme tokyonight")
+    end,
+  })
   use({
     "ms-jpq/coq_nvim",
     branch = "coq",
     run = ":COQdeps",
     requires = { { "ms-jpq/coq.artifacts", branch = "artifacts" } },
+    config = function()
+      global.coq_settings = {
+        auto_start = "shut-up",
+        keymap = { jump_to_mark = "<C-e>" },
+      }
+    end,
+  })
+  use({
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local servers = { "cssls", "html", "stylelint_lsp", "tsserver", "eslint" }
+
+      on_attach = function(client)
+        -- Always use the first formatter
+        client.resolved_capabilities.document_formatting = false
+        -- Plugin: coq_nvim
+        require("coq")().lsp_ensure_capabilities()
+      end
+
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+          on_attach = on_attach,
+        })
+      end
+    end,
+  })
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("null-ls").setup({
+        sources = {
+          require("null-ls").builtins.formatting.prettier,
+          require("null-ls").builtins.formatting.stylua,
+        },
+      })
+    end,
   })
   use({
     "nvim-treesitter/nvim-treesitter",
@@ -67,58 +97,23 @@ require("packer").startup(function()
       require("Comment").setup()
     end,
   })
+  use({ "knubie/vim-kitty-navigator", run = "cp ./*.py ~/.config/kitty/" })
+  use("ggandor/lightspeed.nvim")
+  use("lkemitchll/vim-kitty-runner")
+  use("tpope/vim-surround")
 end)
-
--- LSP
-local lspconfig = require("lspconfig")
-local servers = { "cssls", "html", "stylelint_lsp", "tsserver", "eslint" }
-
-on_attach = function(client)
-  -- Always use the first formatter
-  client.resolved_capabilities.document_formatting = false
-  -- Plugin: coq_nvim
-  require("coq")().lsp_ensure_capabilities()
-end
-
--- Init servers
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-  })
-end
-
-set_keymap("n", "<leader>cd", ":lua vim.lsp.buf.definition()<CR>", {})
-set_keymap("n", "<leader>p", ":lua vim.lsp.buf.formatting()<CR>", {})
-set_keymap("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>", {})
-
--- Theme
-global.tokyonight_style = "night"
-command("colorscheme tokyonight")
 
 -- Keymaps
 set_keymap("n", "<C-]>", ":cnext<CR>", {})
 set_keymap("n", "<C-[>", ":cprevious<CR>", {})
-
--- Plugin: COQ
-global.coq_settings = {
-  auto_start = "shut-up",
-  keymap = { jump_to_mark = "<C-e>" },
-}
-
--- Plugin: neogit
+---- LSP
+set_keymap("n", "<leader>cd", ":lua vim.lsp.buf.definition()<CR>", {})
+set_keymap("n", "<leader>p", ":lua vim.lsp.buf.formatting()<CR>", {})
+---- neogit
 set_keymap("n", "<leader>gs", ":Neogit kind=split<CR>", {})
-
--- Plugin: null_ls (linting & formatting)
-require("null-ls").setup({
-  sources = {
-    require("null-ls").builtins.formatting.prettier,
-  },
-})
-
--- Plugin: telescope.nvim
+---- Telescope
 set_keymap("n", "<C-t>", ":Telescope find_files hidden=true<CR>", {})
 set_keymap("n", "<leader>ag", ":Telescope live_grep<CR>", {})
-set_keymap("n", "<leader>b", ":Telescope oldfiles only_cwd=true<CR>", {})
 set_keymap("n", "<leader>ca", ":Telescope lsp_code_actions<CR>", {})
 set_keymap("n", "<leader>d", ":Telescope diagnostics bufnr=0<CR>", {})
 set_keymap("n", "<leader>v", ":Telescope commands<CR>", {})
