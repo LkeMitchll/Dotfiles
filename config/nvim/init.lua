f1349242fe5efa99cabd039f5755a7558c88c79d
LkeMitchll <mail@luke.fail>
2022-04-01 11:54:38 +0100

    Vim: Tweak global status bar a bit

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -32,7 +32,20 @@ require("packer").startup(function()
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
     config = function()
       require("lualine").setup({
-        options = { globalstatus = true, theme = "tokyonight" },
+        options = {
+          globalstatus = true,
+          theme = "tokyonight",
+          component_separators = { left = "|", right = "|" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
       })
     end,
   })
