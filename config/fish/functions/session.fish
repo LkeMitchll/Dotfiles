4e50657d76c23c0dbda0e1767579a3832b8a0622
LkeMitchll <mail@luke.fail>
2022-08-31 11:39:06 +0100

    Fish: Kitty 'new_window' is deprecated

--- config/fish/functions/session.fish
+++ config/fish/functions/session.fish
@@ -7,7 +7,7 @@ function session
     cd $argv[1]
     kitty-tab $argv[2]
     clear
-    kitty @ new-window --cwd=$PWD --title=server_$argv[2] --keep-focus
+    kitty @ launch --cwd=$PWD --title=server_$argv[2] --keep-focus
     clear
     kitty @ send_text --match=title:server_$argv[2] $argv[3]\u000D
     nvim +KittyOpenRunner
