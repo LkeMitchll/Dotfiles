427bd9372146af1ffe1e7632c8f6e75b261b77a9
LkeMitchll <mail@luke.fail>
2022-09-21 16:27:56 +0100

    Fish: add homebrew to PATH dynamically

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,16 @@
+set brew_path (brew --prefix)
+
 set -e PATH
-set -gx PATH /opt/homebrew/bin $PATH
+set -gx PATH $brew_path/bin $PATH
 set -gx PATH /usr/local/bin /usr/sbin /sbin /usr/bin /bin $PATH
 set -gx PATH ~/.config/nvim/node_modules/.bin ~/.cargo/bin $PATH
 
 # FZF
 set -gx FZF_CTRL_T_COMMAND "rg --hidden --files"
-source (brew --prefix)/opt/fzf/shell/key-bindings.fish; and fzf_key_bindings
+source $brew_path/opt/fzf/shell/key-bindings.fish; and fzf_key_bindings
 
 # ASDF
-source (brew --prefix)/opt/asdf/libexec/asdf.fish
+source $brew_path/opt/asdf/libexec/asdf.fish
 
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/theme.fish
