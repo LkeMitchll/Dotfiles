6756483e032468705dc6185c47fd9278f229b493
LkeMitchll <mail@luke.fail>
2022-09-22 16:28:48 +0100

    Revert 427bd93

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,16 +1,14 @@
-set brew_path (brew --prefix)
-
 set -e PATH
-set -gx PATH $brew_path/bin $PATH
+set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /usr/local/bin /usr/sbin /sbin /usr/bin /bin $PATH
 set -gx PATH ~/.config/nvim/node_modules/.bin ~/.cargo/bin $PATH
 
 # FZF
 set -gx FZF_CTRL_T_COMMAND "rg --hidden --files"
-source $brew_path/opt/fzf/shell/key-bindings.fish; and fzf_key_bindings
+source (brew --prefix)/opt/fzf/shell/key-bindings.fish; and fzf_key_bindings
 
 # ASDF
-source $brew_path/opt/asdf/libexec/asdf.fish
+source (brew --prefix)/opt/asdf/libexec/asdf.fish
 
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/theme.fish
