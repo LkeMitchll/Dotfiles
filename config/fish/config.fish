cb8b804d0af8b6203e7d1b59cc99e5a031d6b760
LkeMitchll <mail@luke.fail>
2022-11-01 13:46:55 +0000

    Fish: Load theme from tokyonight plugin directly

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 set -e PATH
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /usr/local/bin /usr/sbin /sbin /usr/bin /bin $PATH
+set fish_greeting
 
 # FZF
 set -gx FZF_CTRL_T_COMMAND "rg --hidden --files"
@@ -10,4 +11,4 @@ source (brew --prefix)/opt/fzf/shell/key-bindings.fish; and fzf_key_bindings
 source (brew --prefix)/opt/asdf/libexec/asdf.fish
 
 source ~/.config/fish/aliases.fish
-source ~/.config/fish/theme.fish
+source ~/.config/nvim/pack/vish/start/tokyonight.nvim/extras/fish/tokyonight_night.fish
