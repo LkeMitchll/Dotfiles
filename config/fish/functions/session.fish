b1d3b2b546c8bc03603a4406966824d715b6963d
Luke Mitchell <luke@Lukes-Mac-mini.local>
2022-11-02 16:25:25 +0000

    Fish: session function creates a tab title

--- config/fish/functions/session.fish
+++ config/fish/functions/session.fish
@@ -1,14 +1,11 @@
-function kitty-tab
-    kitty @ set-tab-title $argv[1]
-    clear
-end
-
 function session
+    # Usage: session <directory> <command>
+    # e.g.   session ~/Developer/interroban.gg "npm run dev"
     cd $argv[1]
-    kitty-tab $argv[2]
-    clear
-    kitty @ launch --cwd=$PWD --title=server_$argv[2] --keep-focus
-    clear
-    kitty @ send_text --match=title:server_$argv[2] $argv[3]\u000D
+    set tab_name (string split "/" $PWD)[-1]
+
+    kitty @ set-tab-title $tab_name
+    kitty @ launch --cwd=$PWD --title=server_$tab_name --keep-focus
+    kitty @ send_text --match=title:server_$tab_name $argv[2]\u000D
     nvim +KittyOpenRunner
 end
