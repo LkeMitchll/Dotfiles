f36f4deb1f6ffd6af85484d72b9e75ae8f9f1206
Luke Mitchell <luke@Lukes-Mac-mini.local>
2022-11-03 09:41:14 +0000

    Fish: Fix a typo

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ set -gx PATH /opt/homebrew/bin $PATH
 set fish_greeting
 
 # FZF
-set -gx FZF_DEFAULT_OPS "--color=16"
+set -gx FZF_DEFAULT_OPTS "--color=16"
 source (brew --prefix)/opt/fzf/shell/key-bindings.fish; and fzf_key_bindings
 
 # ASDF
