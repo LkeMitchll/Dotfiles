60fdca1490fdd6e97bedd5e5b4d8e61b8b56042c
Luke Mitchell <luke@Lukes-Mac-mini.local>
2022-11-17 13:59:59 +0000

    Fish: Session - Always open repo README

--- config/fish/functions/session.fish
+++ config/fish/functions/session.fish
@@ -7,5 +7,5 @@ function session
     kitty @ set-tab-title $tab_name
     kitty @ launch --cwd=$PWD --title=server_$tab_name --keep-focus
     kitty @ send_text --match=title:server_$tab_name $argv[2]\u000D
-    nvim +KittyOpenRunner
+    nvim +KittyOpenRunner README.md
 end
