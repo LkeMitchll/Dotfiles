bfd1c74c670ff4998d97c8cbff8c150e9865a66f
LkeMitchll <mail@luke.fail>
2022-12-16 14:47:05 +0000

    Vim: Restore <space>gs keymap for neogit

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -120,7 +120,7 @@ keymap("n", "<leader>a", ":Telescope live_grep<CR>", {})
 
 -- Plugin: Neogit
 require("neogit").setup()
-keymap("n", "<leader>g", ":Neogit kind=split<CR>", {})
+keymap("n", "<leader>gs", ":Neogit kind=split<CR>", {})
 
 -- Plugin: kitty-runner
 require("kitty-runner").setup()
