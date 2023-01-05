c3cccbe5708dd6e5e7511c352fa3fd034c9e01b5
LkeMitchll <mail@luke.fail>
2023-01-05 22:45:53 +0000

    Fish: Indicate kitty-runner.nvim windows

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,10 @@
 function fish_prompt
     echo (set_color blue)'╭─' (set_color magenta)(prompt_pwd)
-    echo (set_color blue)'╰─ '(set_color green)'$ '
+    echo (set_color blue)'╰─ '(set_color green)'$ '(set_color normal)
+end
+
+function fish_right_prompt
+    if test -n "$IS_RUNNER" && test $IS_RUNNER = true
+        echo (set_color yellow)"[*]"(set_color normal)
+    end
 end
