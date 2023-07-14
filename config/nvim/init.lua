fa0c4bf88ce8e5e3ed77ec0e0d9b183b7dfdef62
LkeMitchll <mail@luke.fail>
2023-07-14 09:11:22 +0100

    Vim: Update plugins, and configs:
    - Update to lsp-zero v3 (dev)
    - Make use of telescope in neogit
    - Make better use of `opts` in lazy

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,23 +3,29 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.expandtab = true
 vim.opt.shiftround = true
 vim.opt.shiftwidth = 2
-vim.opt.timeoutlen = 200
+vim.opt.listchars:append({ space = "·" })
 
 -- Plugins
-vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
-require("lazy").setup({
+local plugins = {
   {
     "folke/tokyonight.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      vim.cmd.colorscheme("tokyonight-moon")
+    config = function() vim.cmd.colorscheme("tokyonight-moon") end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = "all",
+      highlight = { enable = true }
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      vim.filetype.add({ extension = { njk = "htmldjango" } })
     end
   },
   {
     "echasnovski/mini.nvim",
     init = function()
       require("mini.basics").setup({ options = { extra_ui = true } })
-      vim.opt.listchars:append({ space = "·" })
 
       local mini_modules = {
         "ai", "bracketed", "comment", "files", "jump", "jump2d", "move",
@@ -33,20 +39,8 @@ require("lazy").setup({
     end
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    init = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = "all",
-        highlight = { enable = true }
-      })
-      vim.filetype.add({ extension = { njk = "htmldjango" } })
-    end,
-  },
-  {
     "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim"
-    },
+    dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
       { "<C-T>",   ":Telescope find_files<CR>" },
       { "<C-S-T>", ":Telescope find_files hidden=true<CR>" },
@@ -55,7 +49,7 @@ require("lazy").setup({
   },
   {
     "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
+    branch = "dev-v3",
     dependencies = {
       "neovim/nvim-lspconfig",
       "williamboman/mason.nvim",
@@ -68,25 +62,39 @@ require("lazy").setup({
       "L3MON4D3/LuaSnip",
       "rafamadriz/friendly-snippets",
     },
-    init = function()
-      local lsp = require("lsp-zero").preset("recommended")
-      lsp.on_attach(function(_, bufnr)
-        lsp.default_keymaps({ buffer = bufnr })
+    config = function()
+      local lsp = require("lsp-zero").preset({})
+      lsp.on_attach(function()
+        lsp.default_keymaps({ preserve_mappings = false })
       end)
+
       require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+      require("mason").setup()
+      require("mason-lspconfig").setup({ handlers = { lsp.default_setup } })
+
+      lsp.extend_cmp({ set_extra_mappings = true })
       require("luasnip.loaders.from_vscode").lazy_load()
-      lsp.setup()
+      require("cmp").setup({
+        sources = {
+          { name = "path" },
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "luasnip" }
+        }
+      })
     end
   },
   {
     "NeogitOrg/neogit",
-    config = true,
+    opts = { use_telescope = true },
     keys = { { "<C-G>", ":Neogit kind=split<CR>" } }
   },
   {
     "knubie/vim-kitty-navigator",
     build = "cp ./*.py ~/.config/kitty/"
   },
-  { "lkemitchll/kitty-runner.nvim", config = true },
-  { "folke/which-key.nvim",         config = true }
-})
+  { "lkemitchll/kitty-runner.nvim", config = true }
+}
+
+vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
+require("lazy").setup(plugins, { dev = { path = "~/Developer" } })
