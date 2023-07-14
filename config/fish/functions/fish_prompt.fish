8447563fc209b75480e895c70054ffe1947c9177
LkeMitchll <mail@luke.fail>
2023-07-14 09:08:19 +0100

    Fish: Make the kitty-runner indicator a little more subtle

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
   if test -n "$IS_RUNNER" && test $IS_RUNNER = true
-    set runner_indicator (set_color yellow)"[•]"
+    set runner_indicator (set_color yellow)"•"
   end
   echo (set_color blue)'╭─' (set_color magenta)(prompt_pwd) $runner_indicator
   echo (set_color blue)'╰─' (set_color green)'$ '(set_color normal)
