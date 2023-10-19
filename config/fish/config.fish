cd2f635255cfeb38960d693215d79ea7ba93b480
LkeMitchll <mail@luke.fail>
2023-10-19 09:54:06 +0100

    Fish: Revert to simpler session script

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,8 +11,5 @@ source (brew --prefix fzf)/shell/key-bindings.fish; and fzf_key_bindings
 # RTX
 rtx activate fish | source
 
-# Scripts
-source ~/.config/fish/functions/session.fish
-
 # Theme
 source ~/.local/share/nvim/lazy/tokyonight.nvim/extras/fish/tokyonight_night.fish
