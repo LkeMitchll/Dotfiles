11c96dc8098e612b88e306afc5b9bd1abc194447
LkeMitchll <mail@luke.fail>
2024-01-05 13:10:34 +0000

    rtx has been renamed to mise

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,8 +6,6 @@ fish_add_path /opt/homebrew/bin
 # FZF
 set -gx FZF_DEFAULT_OPTS "--color=16"
 source (brew --prefix fzf)/shell/key-bindings.fish; and fzf_key_bindings
-# RTX
-rtx activate fish | source
 
 # Theme
 source ~/.local/share/nvim/lazy/tokyonight.nvim/extras/fish/tokyonight_night.fish
