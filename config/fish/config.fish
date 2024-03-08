8662e07b0022b6d0b6eb979650d8abe981449885
LkeMitchll <mail@luke.fail>
2024-03-08 09:20:05 +0000

    Setup FZF in the standard way

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,7 +5,7 @@ set fish_greeting
 fish_add_path /opt/homebrew/bin
 # FZF
 set -gx FZF_DEFAULT_OPTS "--color=16"
-source (brew --prefix fzf)/shell/key-bindings.fish; and fzf_key_bindings
+source (brew --prefix fzf)/shell/key-bindings.fish
 
 # Theme
 source ~/.local/share/nvim/site/pack/deps/opt/tokyonight.nvim/extras/fish/tokyonight_night.fish
