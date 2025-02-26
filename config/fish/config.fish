aacf6311a8b0bd5a867de5cf4369b13c0dae1709
LkeMitchll <1729878+LkeMitchll@users.noreply.github.com>
2025-02-26 16:07:09 +0000

    Git & Fish: Tidy up

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,10 @@
 # Disable greeting message
 set fish_greeting
 
-set -gx XDG_CONFIG_HOME ~/.config
-
 # Homebrew
 fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/nvim-macos-arm64/bin
+
 # FZF
 set -gx FZF_DEFAULT_OPTS "--color=16"
 source (brew --prefix fzf)/shell/key-bindings.fish
