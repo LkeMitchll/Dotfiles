06c16059e9a56627e2708807b30cbd8c9c7c54b7
LkeMitchll <1729878+LkeMitchll@users.noreply.github.com>
2025-02-27 09:44:33 +0000

    Fish: more tidying

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,11 +3,10 @@ set fish_greeting
 
 # Homebrew
 fish_add_path /opt/homebrew/bin
-fish_add_path /usr/local/nvim-macos-arm64/bin
 
 # FZF
 set -gx FZF_DEFAULT_OPTS "--color=16"
-source (brew --prefix fzf)/shell/key-bindings.fish
+fzf --fish | source
 
 # Theme
 source ~/.local/share/nvim/site/pack/deps/opt/tokyonight.nvim/extras/fish/tokyonight_night.fish
