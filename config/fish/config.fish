b80a88640f4a464aad94f2666a57ec8965264bec
LkeMitchll <1729878+LkeMitchll@users.noreply.github.com>
2025-02-28 14:56:28 +0000

    Revert "Fish: more tidying"
    This reverts commit 06c16059e9a56627e2708807b30cbd8c9c7c54b7.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,10 +3,11 @@ set fish_greeting
 
 # Homebrew
 fish_add_path /opt/homebrew/bin
+fish_add_path /usr/local/nvim-macos-arm64/bin
 
 # FZF
 set -gx FZF_DEFAULT_OPTS "--color=16"
-fzf --fish | source
+source (brew --prefix fzf)/shell/key-bindings.fish
 
 # Theme
 source ~/.local/share/nvim/site/pack/deps/opt/tokyonight.nvim/extras/fish/tokyonight_night.fish
