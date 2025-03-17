1b7ba3822fd07d4a88be0ed65d3cdcef4d5270bd
LkeMitchll <1729878+LkeMitchll@users.noreply.github.com>
2025-03-17 12:45:08 +0000

    Vim: Remove styleint

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,7 +6,7 @@ vim.opt.shiftwidth = 2
 vim.opt.cmdheight = 0
 
 -- Language Servers (v0.11)
-local lsp_configs = { "biome", "css", "html", "json", "lua", "stylelint", "typescript" }
+local lsp_configs = { "biome", "css", "html", "json", "lua", "typescript" }
 
 for _, config in ipairs(lsp_configs) do
   vim.lsp.enable(config)
@@ -28,7 +28,7 @@ require("nvim-treesitter.configs").setup({
 add({ source = "echasnovski/mini.nvim", depends = { "rafamadriz/friendly-snippets" } })
 
 local mini_modules = {
-  "ai", "basics", "bracketed", "completion", "diff", "files", "git", "icons", "jump",
+  "ai", "basics", "bracketed", "completion", "diff", "files", "icons", "jump",
   "jump2d", "pairs", "pick", "splitjoin", "statusline", "surround", "trailspace"
 }
 
@@ -43,8 +43,10 @@ require("mini.snippets").setup({
 ---- snacks
 add("folke/snacks.nvim")
 require("snacks").setup({
+  gitbrowse = { enabled = true },
   indent = { enabled = true, animate = { enabled = false } },
-  lazygit = { enabled = true }
+  lazygit = { enabled = true },
+  statuscolumn = { enabled = true }
 })
 
 ---- tokyonight
@@ -56,7 +58,6 @@ add("folke/which-key.nvim")
 require("which-key").setup()
 
 -- Keymaps
-vim.keymap.set("n", "<leader>g", ":!git browse<CR>")
 ---- LSP
 vim.keymap.set("n", "<F3>", ":lua vim.lsp.buf.format()<CR>")
 ---- Mini
@@ -65,6 +66,7 @@ vim.keymap.set("n", "<C-P>", ":Pick grep_live<CR>")
 vim.keymap.set("n", "-", ":lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>")
 vim.keymap.set("n", "<BS>", ":lua MiniTrailspace.trim()<CR>")
 ---- snacks
+vim.keymap.set("n", "<leader>g", ":lua Snacks.gitbrowse()<CR>")
 vim.keymap.set("n", "<C-G>", ":lua Snacks.lazygit()<CR>")
 ---- which-key
 vim.keymap.set("n", "`", ":WhichKey<CR>")
