3ea66245465748b3adb79b334cfd7f9fa8612185
LkeMitchll <1729878+LkeMitchll@users.noreply.github.com>
2025-05-12 14:59:22 +0100

    Vim: Add Mason, minify LSP config.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,24 +5,23 @@ vim.opt.expandtab = true
 vim.opt.shiftround = true
 vim.opt.shiftwidth = 2
 vim.opt.cmdheight = 0
+vim.opt.scrolloff = 999
 
 -- Plugins
 require("mini.deps").setup()
 local add = require("mini.deps").add
 
+-- Mason
+add("mason-org/mason.nvim")
+require("mason").setup()
+
 -- Language Servers
 add("neovim/nvim-lspconfig")
-local lsp_configs = { "biome", "cssls", "html", "jsonls", "lua_ls", "ts_ls" }
-
-for _, config in ipairs(lsp_configs) do
-  vim.lsp.enable(config)
-end
-
+vim.lsp.enable({ "biome", "cssls", "html", "jsonls", "lua_ls", "ts_ls" })
 vim.diagnostic.config({ virtual_text = { current_line = true } })
 
 ---- nvim-treesitter
 add("nvim-treesitter/nvim-treesitter")
-
 require("nvim-treesitter.configs").setup({
   ensure_installed = "all",
   highlight = { enable = true }
@@ -30,29 +29,24 @@ require("nvim-treesitter.configs").setup({
 
 ---- mini
 add({ source = "echasnovski/mini.nvim", depends = { "rafamadriz/friendly-snippets" } })
-
 ---- General setup
 local mini_modules = {
   "ai", "basics", "bracketed", "completion", "diff", "files", "icons", "jump",
   "jump2d", "pairs", "pick", "splitjoin", "statusline", "surround", "trailspace"
 }
-
 for _, module in ipairs(mini_modules) do
   require("mini." .. module).setup()
 end
 
 ---- Setup snippets
 local snippets = require("mini.snippets")
-
 snippets.setup({
   snippets = { require("mini.snippets").gen_loader.from_lang() }
 })
-
 snippets.start_lsp_server()
 
 ---- snacks
 add("folke/snacks.nvim")
-
 require("snacks").setup({
   gitbrowse = { enabled = true },
   indent = { enabled = true, animate = { enabled = false } },
