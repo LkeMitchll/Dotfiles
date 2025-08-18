ff325f36548a26207a80e1abe3c1c4b2221761b0
Luke Mitchell <luke@Lukes-MacBook-Pro-2.local>
2025-08-18 18:56:12 +0100

    Fish: tidy up

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,10 +3,12 @@ set fish_greeting
 
 # Homebrew
 fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/.local/bin
 
 # FZF
 set -gx FZF_DEFAULT_OPTS "--color=16"
 source (brew --prefix fzf)/shell/key-bindings.fish
+fzf_key_bindings
 
 # Theme
 source ~/.local/share/nvim/site/pack/core/opt/tokyonight.nvim/extras/fish/tokyonight_night.fish
--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-fish_path="/opt/homebrew/bin/fish"
-brew_path="/opt/homebrew/bin/brew"
+#!/bin/zsh
 # Install dependencies using homebrew
-$brew_path bundle --file ~/.dotfiles/config/homebrew/Brewfile
+/opt/homebrew/bin/brew bundle --file ~/.dotfiles/config/homebrew/Brewfile
 # Change the default shell to fish
-sudo sh -c  "echo ${fish_path} >> /etc/shells"
-chsh -s $fish_path
-# Launch fish and add homebrew to $PATH
-$fish_path -c "fish_add_path /opt/homebrew/bin"
-# Symlink all the dotfiles
-$fish_path -c "rcup -v"
+echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
+chsh -s /opt/homebrew/bin/fish
+# Symlink dotfiles
+/opt/homebrew/bin/rcm/bin/rcup -v
